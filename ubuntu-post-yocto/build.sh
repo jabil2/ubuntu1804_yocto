08b271882798f811424301db98e4c10dba72c585
Murilo Pontes <murilo.pontes@gmail.com>
2020-11-15 13:12:05 +0000

    update sdk_ext

--- ubuntu-post-yocto/build.sh
+++ ubuntu-post-yocto/build.sh
@@ -1,7 +1,9 @@
 
+
+
 mkdir -pv sdk
-cp -av ../yocto/poky_dunfell/build/tmp/deploy/sdk/*.sh sdk/
-cp -av ../yocto/poky_sumo/build/tmp/deploy/sdk/*.sh sdk/
+cp -av ../yocto/poky_dunfell/build/tmp/deploy/sdk/poky-glibc-x86_64-core-image-minimal-aarch64-qemuarm64-toolchain-ext-3.1.3.sh sdk/
+cp -av ../yocto/poky_sumo/build/tmp/deploy/sdk/poky-glibc-x86_64-core-image-minimal-aarch64-toolchain-ext-2.5.3.sh sdk/
 
 docker build . -t murilopontes/ubuntu1804_yocto_sdk
 
--- yocto/sdk-dunfell.sh
+++ yocto/sdk-dunfell.sh
@@ -1,3 +1,3 @@
 cd poky_dunfell
 source oe-init-build-env
-MACHINE=qemuarm64 bitbake core-image-minimal -c populate_sdk
+MACHINE=qemuarm64 bitbake core-image-minimal -c populate_sdk_ext
